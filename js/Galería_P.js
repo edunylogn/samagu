8aedc90b75038b9fd160493bd6817dc16c85445d
Sela2301 <139399187+Sela2301@users.noreply.github.com>
2023-07-25 14:29:40 -0600

    Update Galería_P.js

--- js/Galería_P.js
+++ js/Galería_P.js
@@ -1,12 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const galleryTitle = document.querySelector('.gallery-title');
-    const galleryContent = document.querySelector('.gallery-content');
-  
-    galleryTitle.addEventListener('click', function() {
-      if (galleryContent.style.display === 'none') {
-        galleryContent.style.display = 'block';
-      } else {
-        galleryContent.style.display = 'none';
-      }
-    });
+  const galleryTitle = document.querySelector('.gallery-title');
+  const galleryContent = document.querySelector('.gallery-content');
+
+  galleryTitle.addEventListener('click', function() {
+    galleryContent.classList.toggle('show');
   });
+});
